8cf620b44aa6410b8f4e20bf225e7d5e057c628d
rachel ehrlich <rehrlich@palantir.com>
2014-07-29 16:05:55 -0700

    basic crawler loads locally

--- crawler/wiki_crawler.py
+++ crawler/wiki_crawler.py
@@ -4,29 +4,38 @@ Created July 28, 2014
 @author Adam Campbell, Rachel Ehrlich, Max Fowler
 '''
 
-from database import GRAPHDB
+#from database import GRAPHDB
 import mwparserfromhell
 import pywikibot
+from py2neo import neo4j, cypher
+import re
+
+GRAPHDB = neo4j.GraphDatabaseService()
 
 def load_by_infobox_type(infobox_type):
     pages = get_pages(infobox_type)
+    print len(pages)
+    i = 0
     for page in pages:
-        title = page.title().strip().encode()
-        node = GRAPHDB.get_or_create_indexed_node("TitleIndex", "title", title, {"title": title})
-        node.add_labels(infobox_type)
+        title = page.title().encode("UTF-8")
+        node = add_title_to_db(title)
         text = page.get()
         parsed = mwparserfromhell.parse(text)
         links = parsed.filter_wikilinks()
         for link in links:
-            link_title = extract_title(link)
+            link_title = link.title.encode("UTF-8")
+            adj_node = add_title_to_db(link_title)
+            path = neo4j.Path(node, "links_to", adj_node)
+            path.get_or_create(GRAPHDB)
+        print i
+        i += 1
 
-def extract_title(s):
-    if s is not None:
-        s = re.sub("\[\[","",s)
-        s = re.sub("\]\]", "",s)
-        l = s.split("|")
-        s = l[0]
-    return s
+def add_title_to_db(title, labels=[]):
+    node = GRAPHDB.get_or_create_indexed_node("TitleIndex", "title", title, {"title": title})
+    for label in labels:
+        node.add_labels(label)
+    return node
+
 
 def get_pages(infobox_type):
     site = pywikibot.getSite('en')
@@ -34,3 +43,5 @@ def get_pages(infobox_type):
     pages = list(infobox_template.embeddedin(False, 0))
     return pages
 
+if __name__ == '__main__':
+    load_by_infobox_type("Star Wars character")
