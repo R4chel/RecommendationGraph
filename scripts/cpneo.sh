ee02035da8a67604e1269eb898fea96dd5adc7bf
rachel ehrlich <rehrlich@palantir.com>
2014-07-31 14:18:14 -0700

    modify neo scripts

--- scripts/cpneo.sh
+++ scripts/cpneo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 neo4j stop
 mv /usr/local/Cellar/neo4j/2.1.2/libexec/data /usr/local/Cellar/neo4j/2.1.2/libexec/data.backup
-mkdir /usr/local/Cellar/neo4j/2.1.2/libexec/data
+cp -r $1 /usr/local/Cellar/neo4j/2.1.2/libexec/data
 neo4j start
 
