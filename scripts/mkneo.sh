4ec8820bc58265ca7d656b73ed84ec7663a023af
rachel ehrlich <rehrlich@palantir.com>
2014-07-31 14:25:41 -0700

    fixed mkneo

--- scripts/mkneo.sh
+++ scripts/mkneo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 neo4j stop
-mv /usr/local/Cellar/neo4j/2.1.2/libexec/data /usr/local/Cellar/neo4j/2.1.2/libexec/data.backup
+mv /usr/local/Cellar/neo4j/2.1.2/libexec/data /usr/local/Cellar/neo4j/2.1.2/libexec/data.$1
 mkdir /usr/local/Cellar/neo4j/2.1.2/libexec/data
 neo4j start
 
