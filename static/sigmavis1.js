c27219bc658236c80fc1141176820fdbaf1d82c1
Max Fowler <mfowler@palantir.com>
2014-08-03 23:29:53 -0400

    neighbors

--- static/sigmavis1.js
+++ static/sigmavis1.js
@@ -1,51 +1,79 @@
-function testGraph() {
-    // Let's first initialize sigma:
-    var s = new sigma('container');
-
-    // Then, let's add some data to display:
-    s.graph.addNode({
-        // Main attributes:
-        id: 'n0',
-        label: 'Hello',
-        // Display attributes:
-        x: 0,
-        y: 0,
-        size: 1,
-        color: '#f00'
-    }).addNode({
-        // Main attributes:
-        id: 'n1',
-        label: 'World !',
-        // Display attributes:
-        x: 1,
-        y: 1,
-        size: 1,
-        color: '#00f'
-    }).addEdge({
-        id: 'e0',
-        // Reference extremities:
-        source: 'n0',
-        target: 'n1'
-    });
-
-    // Finally, let's ask our sigma instance to refresh:
-    s.refresh();
-}
-
 function renderGraph() {
     var path_to_file = $("#gexf").data("gexf");
-      sigma.parsers.gexf(
-    path_to_file,
-    { // Here is the ID of the DOM element that
-      // will contain the graph:
-      container: 'container'
-    },
-    function(s) {
-      // This function will be executed when the
-      // graph is displayed, with "s" the related
-      // sigma instance.
-    }
-  );
+    sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+        var k,
+            neighbors = {},
+            index = this.allNeighborsIndex[nodeId] || {};
+
+        for (k in index)
+            neighbors[k] = this.nodesIndex[k];
+
+        return neighbors;
+    });
+
+    sigma.parsers.gexf(
+        path_to_file,
+        {
+            container: 'container'
+        },
+        function(s) {
+            // We first need to save the original colors of our
+            // nodes and edges, like this:
+            s.graph.nodes().forEach(function (n) {
+                //n.color = '#ec5148';
+                n.originalColor = n.color;
+            });
+            s.graph.edges().forEach(function (e) {
+                //e.color = '#ec5148';
+                e.originalColor = e.color;
+            });
+
+            // When a node is clicked, we check for each node
+            // if it is a neighbor of the clicked one. If not,
+            // we set its color as grey, and else, it takes its
+            // original color.
+            // We do the same for the edges, and we only keep
+            // edges that have both extremities colored.
+            s.bind('clickNode', function (e) {
+                var nodeId = e.data.node.id,
+                    toKeep = s.graph.neighbors(nodeId);
+                toKeep[nodeId] = e.data.node;
+
+                s.graph.nodes().forEach(function (n) {
+                    if (toKeep[n.id])
+                        n.color = n.originalColor;
+                    else
+                        n.color = '#eee';
+                });
+
+                s.graph.edges().forEach(function (e) {
+                    if (toKeep[e.source] && toKeep[e.target])
+                        e.color = e.originalColor;
+                    else
+                        e.color = '#eee';
+                });
+
+                // Since the data has been modified, we need to
+                // call the refresh method to make the colors
+                // update effective.
+                s.refresh();
+            });
+
+            // When the stage is clicked, we just color each
+            // node and edge with its original color.
+            s.bind('clickStage', function (e) {
+                s.graph.nodes().forEach(function (n) {
+                    n.color = n.originalColor;
+                });
+
+                s.graph.edges().forEach(function (e) {
+                    e.color = e.originalColor;
+                });
+
+                // Same as in the previous event:
+                s.refresh();
+            });
+        });
 }
 
 $(document).ready(function() {
